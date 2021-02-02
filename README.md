55e8132edf834faa279d68aafaacc4c8eca9ef58
Chris Lamb <chris.lamb13@hotmail.com>
2021-02-02 10:35:12 -0500

    Update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Fitness_Tracker
+Fitness Tracker is a web app that allows users to create complete workouts by adding exercises to them. Then the app stores the users workout where the stats are then displayed to the screen
+
+## Installation
+After pulling from the repository, in the terminal, type 
+```bash
+npm install
+```
+## Usage
+This app tracks a users workout information so they have a simplifed way of keeping track of their information.
+
+## Links
---
+++ server.js
@@ -0,0 +1,132 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerdb",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const databaseUrl = "workoutTrackerdb";
+const collections = ["workouts"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", (error) => {
+  console.log("Database Error:", error);
+});
+
+//Get Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find()
+    .sort({ x: -1 })
+    .toArray(function (err, result) {
+      if (err) throw err;
+
+      res.json(result);
+      console.log(result);
+    });
+});
+
+app.get("/api/workouts", (req, res) => {
+  let collection = db.collection("Workout");
+  collection.find({}, (error, found) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(found);
+    }
+  });
+});
+
+//Post Routes
+app.post("/api/workouts", (req, res) => {
+  console.log(req.body);
+
+  db.Workout.save(req.body, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+//Put Routes
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.update(
+    { _id: mongojs.ObjectId(req.params.id) },
+
+    { $push: { exercises: req.body } },
+
+    (error, saved) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(saved);
+      }
+    }
+  );
+
+  db.Workout.update(
+    { _id: mongojs.ObjectId(req.params.id) },
+    {
+      $currentDate: { day: { $type: "date" } },
+    },
+
+    (error, saved) => {
+      if (error) {
+        console.log(error);
+      } else {
+      }
+    }
+  );
+
+  db.Workout.update(
+    { _id: mongojs.ObjectId(req.params.id) },
+    {
+      $inc: { totalDuration: req.body.duration },
+    },
+    {
+      $currentDate: { day: { $type: "date" } },
+    },
+
+    (error, saved) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.send(saved);
+      }
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000!");
+});
