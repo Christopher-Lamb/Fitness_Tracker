368849e0c3249e6400450a7a10a83784f7401e16
Chris Lamb <chris.lamb13@hotmail.com>
2021-02-02 11:30:13 -0500

    Update app.listen

--- server.js
+++ server.js
@@ -127,6 +127,6 @@ app.put("/api/workouts/:id", (req, res) => {
   );
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("App running on port 3000!");
 });
